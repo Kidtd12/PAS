512e68d5ed87d19ae7d9f715549c7a0e601b924a
RozaNega <rozanega15@gmail.com>
2026-03-07 10:14:11 +0300

    push folders

---
+++ PAS.Domain/Catalog/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Common;
+
+namespace Domain.Catalog
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; private set; }
+
+        public Guid? ParentCategoryId { get; private set; }
+
+        public string Description { get; private set; }
+
+        public Category ParentCategory { get; private set; }
+
+        public ICollection<Category> SubCategories { get; private set; } = new List<Category>();
+
+        public Category(string name, string description, Guid? parentId = null)
+        {
+            Name = name;
+            Description = description;
+            ParentCategoryId = parentId;
+        }
+    }
+}
---
+++ PAS.Domain/Common/DocumentAttachment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Domain.Common
+{
+    public class DocumentAttachment : BaseEntity
+    {
+        public string FileName { get; private set; }
+
+        public string FilePath { get; private set; }
+
+        public string ContentType { get; private set; }
+
+        public Guid RelatedEntityId { get; private set; }
+
+        public string RelatedEntityName { get; private set; }
+
+        public DocumentAttachment(string fileName, string filePath, string type, Guid entityId, string entityName)
+        {
+            FileName = fileName;
+            FilePath = filePath;
+            ContentType = type;
+            RelatedEntityId = entityId;
+            RelatedEntityName = entityName;
+        }
+    }
+}
