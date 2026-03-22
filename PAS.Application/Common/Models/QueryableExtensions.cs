3a683fdd4ea6db12d6b79c662da8f456c03e507c
Kidtd12 <Kidtd12@users.noreply.github.com>
2026-03-22 15:31:23 +0300

    Remove Application events and global/common helper files

--- PAS.Application/Common/Models/QueryableExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace Application.Common.Models;
-
-public static class QueryableExtensions
-{
-    public static Task<PaginatedList<T>> PaginatedListAsync<T>(this IQueryable<T> queryable, int pageNumber, int pageSize, CancellationToken cancellationToken = default)
-    {
-        return PaginatedList<T>.CreateAsync(queryable, pageNumber, pageSize, cancellationToken);
-    }
-}
--- PAS.Application/Common/Security/AuthorizeAttribute.cs
+++
@@ -1,9 +0,0 @@
-namespace Application.Common.Security;
-
-[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
-public sealed class AuthorizeAttribute : Attribute
-{
-    public string Roles { get; init; } = string.Empty;
-    public string Policy { get; init; } = string.Empty;
-    public string Permissions { get; init; } = string.Empty;
-}
