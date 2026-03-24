047d0aabe245b9c1ed36db9548b0b0ea9af425f2
Kidtd12 <Kidtd12@users.noreply.github.com>
2026-03-24 11:08:02 +0300

    Fix duplicate handlers/DTO conflicts and align validation/auth DTOs

--- PAS.Application/Features/Receiving/Inspections/Commands/CreateInspection/CreateInspectionValidator.cs
+++ PAS.Application/Features/Receiving/Inspections/Commands/CreateInspection/CreateInspectionValidator.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
 
-namespace PAS.Application.Features.Receiving.Inspections.Commands.CreateInspection
+namespace Application.Features.Receiving.Inspections.Commands.CreateInspection;
+
+public class CreateInspectionValidator : AbstractValidator<CreateInspectionCommand>
 {
-    internal class CreateInspectionValidator
+    public CreateInspectionValidator()
     {
+        RuleFor(v => v.ReceivingNoteId)
+            .NotEmpty().WithMessage("Receiving note is required.");
+
+        RuleFor(v => v.Items)
+            .NotEmpty().WithMessage("At least one item must be inspected.");
+
+        RuleForEach(v => v.Items).ChildRules(item =>
+        {
+            item.RuleFor(i => i.ItemId)
+                .NotEmpty().WithMessage("Item is required.");
+
+            item.RuleFor(i => i.ReceivedQuantity)
+                .GreaterThan(0).WithMessage("Received quantity must be greater than 0.");
+
+            item.RuleFor(i => i.AcceptedQuantity)
+                .GreaterThanOrEqualTo(0).WithMessage("Accepted quantity must be greater than or equal to 0.");
+
+            item.RuleFor(i => i.RejectedQuantity)
+                .GreaterThanOrEqualTo(0).WithMessage("Rejected quantity must be greater than or equal to 0.");
+
+            item.RuleFor(i => i.AcceptedQuantity + i.RejectedQuantity)
+                .Equal(i => i.ReceivedQuantity)
+                .WithMessage("Accepted quantity plus rejected quantity must equal received quantity.");
+        });
+
+        RuleFor(v => v.DeviationNotes)
+            .MaximumLength(1000).WithMessage("Deviation notes must not exceed 1000 characters.");
     }
 }
